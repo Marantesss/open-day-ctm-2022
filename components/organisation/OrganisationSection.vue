edc8433795600aad18e7a6eb558dba67164e0cc3
Gonçalo Marantes <up201706917@edu.fe.up.pt>
2022-03-18 18:06:37 +0000

    :rabbit: add easter egg

--- components/organisation/OrganisationSection.vue
+++ components/organisation/OrganisationSection.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="organisation" class="container mx-auto my-6 px-2">
-    <h1 class="text-4xl uppercase mb-4 text-ctm-dark-blue font-section-title">/Organisation</h1>
+    <h1 class="text-4xl uppercase mb-4 text-ctm-dark-blue font-section-title">
+      /Organisation
+    </h1>
     <div class="flex flex-wrap items-start justify-center">
       <OrganisationCard
         class="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 w-full"
@@ -11,6 +13,31 @@
         :cargo="cargo"
         :img="img"
       />
+      <div
+        class="
+          xl:w-1/5
+          lg:w-1/4
+          md:w-1/3
+          sm:w-1/2
+          w-full
+          flex flex-col
+          justify-center
+          items-center
+        "
+      >
+        <img
+          v-if="marantes"
+          :src="`images/organisation/goncalo-marantes.jpg`"
+          class="rounded-full h-32 w-32 object-cover transform animate-bounce"
+        />
+        <div class="text-ctm-dark-blue text-center p-2">
+          <h2 class="text-base">
+            Gonçalo Maran<span @click="toggleMarantes">t</span>es
+          </h2>
+          <h4 class="text-sm">Member @ IEEE UP SB</h4>
+          <h4 class="text-lg font-title">Webmaster</h4>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +49,7 @@ export default {
   components: { OrganisationCard },
   data() {
     return {
+      marantes: false,
       organisation: [
         {
           name: 'Nuno Paulino',
@@ -53,13 +81,21 @@ export default {
           cargo: 'Multimedia Communications Technologies',
           img: 'luis-aly.png',
         },
+        /*
         {
           name: 'Gonçalo Marantes',
           title: 'Member @ IEEE UP SB',
           cargo: 'Webmaster',
         },
+        */
       ],
     }
+  },
+
+  methods: {
+    toggleMarantes() {
+      this.marantes = !this.marantes
+    },
   },
 }
 </script>
